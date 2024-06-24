7ccdcf53682c5482ca48579740f3ce44bbc49f7e
aiman <harithaiman2299@gmail.com>
2024-06-24 08:48:27 +0800

    test

---
+++ arraytest.js
@@ -0,0 +1,19 @@
+/* let oddNum = [1,3,5];
+let evenNum = [2,4,6];
+let allNum = oddNum.concat(evenNum);
+allNum.sort()
+
+console.log(allNum);
+console.log(evenNum) */
+
+/*let num = [2,5,7,8,1,9];
+let sortedNum = num.sort();
+console.log(sortedNum); //contains sorted array
+console.log(num); // the original array is also sorted
+
+let num2 = [2,5,7,8,1,9];
+let sortedNum2 = [...num].sort(); //using this, it will create a soft copy of the original array and the original array wont be sorted
+console.log(sortedNum2); 
+console.log(num2); */
+
+
